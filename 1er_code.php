c7cb4eb4bed69eb6da1ec0db042a9bdead5f9921
ulrich <ulrichfotso10@gmail.com>
2023-03-21 12:18:19 +0100

    feat

---
+++ 1er_code.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>le 1er code php</title>
+</head>
+<body>
+	<p> ceci est un paragraphe </p>
+
+	<?php
+		echo "BONJOUR";
+	?> 
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,9 @@
+<?php
+// echo $_GET['nom'];
+require('database.php');
+
+// $array = [];
+// function verif($max, $min, $message, $array) {
+//     if ($name < )
+// }
+// ?>
